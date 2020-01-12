51fe21f433eb234a8f615074ebff5813cefbc99b
Jan-Niklas Meier <jan@jansblog.org>
2020-01-12 17:56:18 +0100

    Adding example usage file and providing installation instructions in the readme.

--- README.md
+++ README.md
@@ -6,9 +6,40 @@
 
 A Java implementation of the Homie Convention (https://github.com/homieiot/convention).
 
-WIP and not all features are currently supported.
+The implementation is WIP but already usable. The core features are implemented.
 
-ToDo
+Features that are currently supported:
+* Basic device behavior and state machine
+* Required homie attributes
+* Nodes and properties
+* Settable properties
+
+Installation and Usage
 ====
 
-* Add example code
+Currently, no builds are uploaded to a build server. You will have to add the following lines to your gradle
+project configuration to ensure that homie-java is built from source together with your project:
+
+build.gradle:
+```
+dependencies {
+    implementation('io.github.dschanoeh:homie-java') {
+        version {
+            branch = 'master'
+        }
+    }
+}
+```
+
+settings.gradle:
+```
+sourceControl {
+    gitRepository("https://github.com/dschanoeh/homie-java.git") {
+        producesModule("io.github.dschanoeh:homie-java")
+    }
+}
+```
+
+
+See [ExampleUsage.java](https://github.com/dschanoeh/homie-java/blob/master/src/test/java/io/github/dschanoeh/homie_java/ExampleUsage.java)
+for an example how the homie client can be used.
---
+++ src/test/java/io/github/dschanoeh/homie_java/ExampleUsage.java
@@ -0,0 +1,48 @@
+package io.github.dschanoeh.homie_java;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ExampleUsage {
+
+    @Test
+    void run() throws InterruptedException {
+        /* Create a new configuration object with the basic settings to allow homie to connect to the MQTT broker */
+        Configuration c = new Configuration();
+        c.setBrokerUrl("tcp://127.0.0.1:1883");
+        c.setDeviceID("example-device");
+        c.setDeviceName("A Homie example device");
+
+        /* Create a new homie instance with the configuration */
+        Homie homie = new Homie(c, "My Test Firmware", "0.0.1");
+
+        /* Before homie is started, all nodes and properties must be created so they are known during initialization. */
+        Node node = homie.createNode("weather-station", "weather-station");
+        node.setName("My weather station");
+
+        Property temperatureProperty = node.getProperty("temperature");
+        temperatureProperty.setUnit("°C");
+        temperatureProperty.setDataType(Property.DataType.FLOAT);
+
+        Property humidityProperty = node.getProperty("humidity");
+        humidityProperty.setUnit("%");
+        humidityProperty.setDataType(Property.DataType.INTEGER);
+
+        /* Start homie. This call is non-blocking and new threads will be created for the main state machine handling
+         * connecting and disconnecting.
+         */
+        homie.setup();
+
+        /* Wait till homie has connected to the broker and is done initializing */
+        while(homie.getState() != Homie.State.READY) {
+            Thread.sleep(50);
+        }
+
+        /* your code here to post sensor values, etc. */
+        humidityProperty.send(12);
+        temperatureProperty.send(22.546651, 2);
+
+        homie.shutdown();
+    }
+}
